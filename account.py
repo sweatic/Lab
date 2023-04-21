314fd69a4435844787f0e5f54810d19a92e4c73e
sweatic <therealsweatic@gmail.com>
2023-04-21 18:57:43 -0500

    First Commit and Push

--- account.py
+++ account.py
@@ -1,2 +1,19 @@
-class Account()
-    def __init__(self):
+class Account:
+    def __init__(self, name:str) -> None:
+        '''
+        Function sets up instance of an object
+        :param name: Account name
+        '''
+    pass
+
+    def deposit(self, amount):
+        pass
+
+    def withdrawl(self, amount):
+        pass
+
+    def get_balance(self):
+        pass
+
+    def get_name(self):
+        pass
